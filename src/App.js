dd1711eee49805ebf4cce8dfe944b6b11e414b00
Barun Debnath <barundebnath91@gmail.com>
2022-01-20 20:55:26 +0530

    grid card done

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img src={card.src} className="front" alt="card front" />
+              <img src="/img/cover.png" alt="card back" className="back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
