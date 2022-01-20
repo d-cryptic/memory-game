2e1fe300da28c4ad304e4c9aee9ef2aa83dea37d
Barun Debnath <barundebnath91@gmail.com>
2022-01-21 00:29:41 +0530

    card choices

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./SingleCard.css";
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className="card">
       <div>
         <img src={card.src} className="front" alt="card front" />
-        <img src="/img/cover.png" alt="card back" className="back" />
+        <img
+          src="/img/cover.png"
+          alt="card back"
+          onClick={handleClick}
+          className="back"
+        />
       </div>
     </div>
   );
